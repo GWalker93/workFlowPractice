0b91a1babef6a11192eba53195f00ef7729c30df
Gerald Walker <gerald.e.walker05@gmail.com>
2025-11-19 12:36:07 -0600

    made for varb

--- practice/src/Demo.java
+++ practice/src/Demo.java
@@ -2,6 +2,12 @@ public class Demo {
 
     public static void main(String[] args) {
 
-
+        /**
+         * Made Variables
+         * @GeraldWalker
+         */
+        int num1;
+        int num2;
+        int total =0;
     }
 }
